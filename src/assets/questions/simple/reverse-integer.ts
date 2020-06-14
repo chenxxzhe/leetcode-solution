98188ff2cd43fce4c176df23965d00eefd2640bd
chenzhe <jacky.chen@coatesgroup.com>
2020-06-14 21:50:58 +0800

    feat: reverse-integer

--- src/assets/questions/simple/reverse-integer.ts
+++ src/assets/questions/simple/reverse-integer.ts
@@ -23,12 +23,37 @@ const config: QuestionConfig = {
   desc,
   category: 'algorithm',
   solutions: {
-    default(num: number): number {
-      return num
+    default(x: number): number {
+      let str = x.toString()
+      const neg = str[0] === '-'
+      if (neg) {
+        str = str.substring(1)
+      }
+      const rev = + str.split('').reverse().join('')
+      if (rev >= Math.pow(2, 31)) { return 0}
+      return neg ? - rev :  rev
+    },
+    move(x: number): number {
+      let ret = 0
+      while (x !== 0) {
+        // every loop push x end-side number to ret
+        // ret * 10 move current number award
+        // x % 10, push x end-side number
+        ret = ret * 10 + x % 10
+        // cut end-side
+        // x | 0 to round number
+        x = (x / 10) | 0
+      }
+      // check overflow
+      return (ret | 0) === ret ? ret : 0
     },
   },
   testCase: [
+    [[123], 321],
+    [[-123], -321],
+    [[1534236469], 0],
   ],
+  comment: '使用运算方式, 搬运数字, 而不是使用字符串的方法.',
 }
 
 export default config
