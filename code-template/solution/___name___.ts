611256281c41717e5c31a4aea75e0ca1d4a3f7d0
chenzhe <jacky.chen@coatesgroup.com>
2020-06-17 08:43:10 +0800

    chore: code template

---
+++ code-template/solution/___name___.ts
@@ -0,0 +1,16 @@
+const desc = `
+`
+
+const config: QuestionConfig = {
+  id: 20,
+  desc,
+  category: 'algorithm',
+  solutions: {
+    default(s: string): string {
+    },
+  },
+  testCase: [
+  ],
+}
+
+export default config
